9262556b254bcf2cc6e2484fb265a0fd5709e8a5
Sergei Mozhaisky <sergei.mozhaisky@gmail.com>
2013-10-16 13:24:03 +0000

    perlgolf-08 entry v 3.0

--- script/technix.pl
+++ script/technix.pl
@@ -1,2 +1,2 @@
-#!perl -ln0
-sub h{$_=pop;while(/(\d)(\s*)(?=\d)/g){$f=$1;$s=$2;$'=~/./;s/$f$s$&/ $s / if$f+$&==10||$f==$&}$_}@r=0..(s/\n//g)-1;$o=$_;while($,ne$o){$,=$o;for$i(0..8){$e[$i]=h join'',map{substr$,,$i+$_*9,1}@r}$,='';for$i(@r){$,.=substr$_,$i,1for@e}$o=h$,}/\d/?print:0for$o=~/.{9}/g
+#!perl -np0
+sub h{my$_=pop;$f=$&,$'=~/\d/,$f+$&==10||$f==$&?s/$f(\D*)$&/ $1 /:0while/\d/g;$_}while($,ne$_){my@e;$e["@-"%10].=$&while/./gs;$,='';for$i(0..y/\n//){$,.=substr h($_),$i,1for@e}$_=h$,;s/ {9}\n//}
